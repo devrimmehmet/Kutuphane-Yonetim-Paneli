c0c29369fb630b142f482b99604360caf1ebcbf8
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2020-02-16 00:15:19 +0300

    MVC and Category...
    Model View Controller ve Kategoriler kısmı yazıldı
    Yazar İşlemlerine başlandı Yazar güncelleme işlemi kaldı sadece.

---
+++ Controllers/WriterController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Devrekani_Sehitler_Kutuphanesi.Models.Entity;
+namespace Devrekani_Sehitler_Kutuphanesi.Controllers
+{
+    public class WriterController : Controller
+    {
+        // GET: Writer
+        DB_Kutuphane_Entities db = new DB_Kutuphane_Entities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBL_YAZAR.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YazarEkle()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YazarEkle(TBL_YAZAR p)
+        {
+            db.TBL_YAZAR.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult YazarSil(int id)
+        {
+            var yazar = db.TBL_YAZAR.Find(id);
+            db.TBL_YAZAR.Remove(yazar);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
