08b89f54f9d76ac3f575603edb308f6b750cdf54
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2020-02-18 23:09:14 +0300

    Personel Modülü
    Personel modülü yazıldı.
    Staff Module writer.

--- Models/Entity/TBL_PERSONEL.cs
+++ Models/Entity/TBL_PERSONEL.cs
@@ -11,10 +11,13 @@ namespace Devrekani_Sehitler_Kutuphanesi.Models.Entity
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class TBL_PERSONEL
     {
         public byte ID { get; set; }
+
+        [Required(ErrorMessage="Personel Adý Boþ Býrakýlamaz")]
         public string PERSONEL { get; set; }
     }
 }
