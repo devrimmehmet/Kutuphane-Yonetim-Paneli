dcb90bccaa3c91c56e14f179be66ab18778dcbb3
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2020-02-24 03:56:22 +0300

    Just Student Personel
    Sadece Öğrenci Paneli Kaldı...

---
+++ Controllers/UyeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Devrekani_Sehitler_Kutuphanesi.Models.Entity;
+namespace Devrekani_Sehitler_Kutuphanesi.Controllers
+{
+    public class UyeController : Controller
+    {
+        DB_Kutuphane_Entities2 db = new DB_Kutuphane_Entities2();
+        public ActionResult GirisYap()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult KayitOl()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult KayitOl(TBL_UYELER p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("KayitOl");
+            }
+            db.TBL_UYELER.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
